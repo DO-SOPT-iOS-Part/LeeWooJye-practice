f564b0d7a599510f675dc5b94fe07d1f306ea3f3
leewoojye <whitehole36@naver.com>
2023-10-07 14:08:09 +0900

    Initial Commit

---
+++ sopt_first_seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt_first_seminar
+//
+//  Created by Woo Jye Lee on 10/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
